405bbcd1617e7da18516fd86826d59cfc7e209ed
ToriNtHr <toma.ziliute@stud.viko.lt>
2022-12-09 15:20:02 +0200

    Had to re-do everything!

---
+++ Mobilioji2/app/src/main/java/com/example/projectnr2try/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.projectnr2try
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.projectnr2try.ui.theme.ProjectNr2TryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProjectNr2TryTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ProjectNr2TryTheme {
+        Greeting("Android")
+    }
+}
--- PROJECT/app/src/main/java/com/example/project/domain/model/Recipe.kt
+++
@@ -1,9 +0,0 @@
-package com.example.project.domain.model
-
-data class Recipe(
-    var id: String?=null,
-    var name:String?=null,
-    var catagory:String?=null,
-    var steps:String?=null,
-    var ingredients:String?=null,
-)
