8dec8cfcf1053f3e64feec034146eb41553a7cc6
ToriNtHr <toma.ziliute@stud.viko.lt>
2022-12-10 17:15:14 +0200

    hilt is working now idk why

---
+++ Mobilioji2/app/src/main/java/com/example/projectnr2try/presentation/Recipes/RecipeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.projectnr2try.presentation.Recipes
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun RecipeScreen(
+    viwmodel: RecipesViewModel= hiltViewModel()
+)
+{
+
+}
